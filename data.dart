ba9cc06fdd01ee6c3c8a1567efcb7e44bce93399
Akhmad arip <akhmadarip86@gmail.com>
2025-03-06 14:46:03 +0700

    Create data.dart

---
+++ data.dart
@@ -0,0 +1,98 @@
+final List<Map<String, dynamic>> produkData = [
+  {
+    
+    "name": "Spider-Man",
+    "price": "Rp. 10.000.000",
+    "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Encanto",
+    "price": "Rp. 10.000.000",
+    "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BNjE5NzA4ZDctOTJkZi00NzM0LTkwOTYtMDI4MmNkMzIxODhkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Red Notice",
+   "price": "Rp. 10.000.000",
+   "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BZmRjODgyMzEtMzIxYS00OWY2LTk4YjUtMGMzZjMzMTZiN2Q0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "The Commando",
+    "price": "Rp. 10.000.000",
+    "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BYTQ5ZjM3YmQtNTQ4ZC00NzBmLTk5YTUtN2I1NWRhYzcyNzgzXkEyXkFqcGdeQXVyNDA1NDA2NTk@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Blacklight",
+    "price": "Rp. 10.000.000",
+    "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BY2ViYmNhNzEtZDJhNS00YzM4LTkzZWItZDJiNzc5ZDExOWZmXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Turning Red",
+    "price": "Rp. 10.000.000",
+   "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BNjY0MGEzZmQtZWMxNi00MWVhLWI4NWEtYjQ0MDkyYTJhMDU0XkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Kimi",
+    "price": "Rp. 10.000.000",
+    "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BYjFmOWUwYjgtM2UyYS00M2FmLTgwNmUtMWIwNTc2ZTgzNmRhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "The Ice Age",
+    "price": "Rp. 10.000.000",
+   "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BNDJmMzQyMzAtMzMxMy00NTI3LTgzOGMtZDU3Yzc4MjRjNzkwXkEyXkFqcGdeQXVyMTA1OTcyNDQ4._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Transylvania 4",
+    "price": "Rp. 10.000.000",
+   "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BZWM4MzUyYTAtMGFiYy00MmRkLWIyNDktMWFlMDlmZjJmNWZmXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Eternals",
+    "price": "Rp. 10.000.000",
+   "isFavorite": false,
+   "image":
+      "https://m.media-amazon.com/images/M/MV5BMTExZmVjY2ItYTAzYi00MDdlLWFlOWItNTJhMDRjMzQ5ZGY0XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_SX300.jpg",
+  },
+  {
+    
+    "name": "Nightmare Alley",
+   "price": "Rp. 10.000.000",
+   "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BOTI4NDhhNGEtZjQxZC00ZTRmLThmZTctOGJmY2ZlOTc0ZGY0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+  },
+  {
+    
+    name: "Texas Chainsaw",
+    "price": "Rp. 10.000.000",
+   "isFavorite": false,
+    "image":
+      "https://m.media-amazon.com/images/M/MV5BMDU4Yzk1NmUtNDU4OC00YzVmLTgwOTUtYTY2NjNmMjY5YjVmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  },
+];
